3e4bf0d57d445f8529a77b15fd668d19628a19e5
smraikai <twyman214+github@gmail.com>
2024-09-30 21:40:07 -0400

    redirects for old pages

---
+++ app/Http/Middleware/RedirectMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $redirects = config('redirects');
+        $currentPath = $request->path();
+
+        if (isset($redirects[$currentPath])) {
+            return redirect($redirects[$currentPath], 301);
+        }
+
+        return $next($request);
+    }
+}
