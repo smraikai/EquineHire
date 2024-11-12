2bf9b2ee4f2e9f35829468a74f4fe49a522ba39c
smraikai <twyman214+github@gmail.com>
2024-11-11 19:45:27 -0500

    Fixed typo in dashboard

--- resources/views/dashboard/job-listings/edit.blade.php
+++ resources/views/dashboard/job-listings/edit.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @php
-    $metaTitle = 'Edit Employer Profile | EquineHire';
-    $pageTitle = 'Edit Employer Profile';
+    $metaTitle = 'Edit Job Listing | EquineHire';
+    $pageTitle = 'Edit Job Listing';
 @endphp
 
 @section('content')
-    <div class="container py-12 mx-auto sm:py-24 px-4">
+    <div class="container px-4 py-12 mx-auto sm:py-24">
         <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <form action="{{ route('employers.job-listings.update', $jobListing) }}" method="POST"
                 enctype="multipart/form-data" class="p-6 m-4 bg-white rounded-lg shadow-sm sm:m-0">
                 @csrf
                 @method('PUT')
-                @include('dashboard.job-listings._form', ['submitButtonText' => 'Update Employer Profile'])
+                @include('dashboard.job-listings._form', ['submitButtonText' => 'Update Job Listing'])
             </form>
         </div>
     </div>
